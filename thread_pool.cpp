8d161dc9994073220bbd2d8c148b0dfd0b18e132
thangdinh1298 <dinhthang6c@gmail.com>
2021-03-05 15:43:47 +0700

    Returned response

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <cstring>
+
 #include "thread_pool.h"
+#include "io_helper.h"
 
 ThreadPool::ThreadPool(unsigned thread_num, unsigned connection_buffer_len): 
                        max_connection_buffer_len_(connection_buffer_len) {
    for (unsigned int i = 0; i < thread_num; i++) {
       std::thread t([this]() {
-				std::cout << "Performing" << std::endl;
-				std::unique_lock<std::mutex> lk(mu_); //automatically unlock when goes out of scope
-				cv_.wait(lk, [this](){return !connection_buffer_.empty();});
-				auto next_task = connection_buffer_.front();
-				connection_buffer_.pop();
-				std::cout << next_task << std::endl;
+            char buf[1024];
+            char response[] = "Hello World\n";
+            while (true) {
+               std::unique_lock<std::mutex> lk(mu_); //automatically unlock when goes out of scope
+               while (connection_buffer_.empty()) {
+                  cv_.wait(lk);
+               }
+               std::cout << "Child Thread: Buffer size: " << connection_buffer_.size() << std::endl;
+               auto next_task = connection_buffer_.front();
+               connection_buffer_.pop();
+               
+               //Write response
+               sprintf(buf, ""
+                   "HTTP/1.0 200 OK\r\n"
+                   "Server: OSTEP WebServer\r\n"
+                   "Content-Length: %d\r\n"
+                   "Content-Type: %s\r\n\r\n",
+                   strlen(response), "text");
+               write_or_die (next_task, buf, strlen(buf));
+               write_or_die (next_task, response, strlen(response));
+            }
       });
       thread_buffer_.push_back(std::move(t));
    }
--- thread_pool.h
+++ thread_pool.h
@@ -6,22 +6,18 @@
 #include <condition_variable>
 #include <iostream>
 
-#include "task.h"
+//#include "task.h"
 
 class ThreadPool {
 public:
    ThreadPool(unsigned thread_num=4, unsigned connection_buffer_len=100);
-	bool connection_buffer_full() {
-		std::lock_guard<std::mutex> lk(mu_);
-		return max_connection_buffer_len_ == connection_buffer_.size();
-	}
 
 	void insert_conn(int conn_fd) {
+      std::cout << "MainThread: Inserting" << std::endl;
 		std::unique_lock<std::mutex> lk(mu_);
-		while (!connection_buffer_full()) {
+		while (max_connection_buffer_len_ == connection_buffer_.size()) {
 			cv_.wait(lk);
 		}
-		std::cout << "Inserting" << std::endl;
 		connection_buffer_.push(conn_fd);
 		cv_.notify_one();
 	}
